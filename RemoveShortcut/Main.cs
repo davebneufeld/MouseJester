9a47a4bcccbf551853cddec309afec567b95f231
Jonathan <qaovxtazypdl@hotmail.com>
2015-05-13 17:39:06 -0400

    now refreshes desktop icons to prevent shortcut lingering on install for all users.

--- RemoveShortcut/Main.cs
+++ RemoveShortcut/Main.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
 
 namespace RemoveShortcut
 {
     class RemoveShortcut
     {
+        [DllImport("Shell32.dll")]
+        private static extern int SHChangeNotify(int eventId, int flags, IntPtr item1, IntPtr item2);
+
         //takes in the path where the shortcut is located as argument.
         static void Main(string[] args)
         {
@@ -17,6 +22,8 @@ namespace RemoveShortcut
             string dir = string.Join(" ", args);
             const string shortcutName = "MouseJester.lnk";
             File.Delete(Path.Combine(dir, shortcutName));
+
+            SHChangeNotify(0x8000000, 0x1000, IntPtr.Zero, IntPtr.Zero);
         }
     }
 }
